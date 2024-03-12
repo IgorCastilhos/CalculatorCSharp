04ca1d25ad7c74abe068727ba5c5215f6da451ac
igorc <igorcastilhos2010@hotmail.com>
2024-03-12 12:41:59 -0300

    Added new methods and refactored the code

--- Program.cs
+++ Program.cs
@@ -6,41 +6,114 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.Clear();
+            Menu();
+        }
 
-            Console.WriteLine("Welcome to the Calculator!");
+        static void Menu()
+        {
+            // Faça uma validação para que o usuário não consiga digitar um valor maior que 5 antes
+            Console.Clear();
+            Console.WriteLine("Calculator");
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1 - Sum");
+            Console.WriteLine("2 - Subtraction");
+            Console.WriteLine("3 - Multiplication");
+            Console.WriteLine("4 - Division");
+            Console.WriteLine("5 - Exit");
+            Console.WriteLine("-------------");
+            Console.WriteLine("Enter the option:");
+            string input = Console.ReadLine();
+            short option;
+            Console.WriteLine();
+
+            if (!short.TryParse(input, out option) || option > 5 || option < 1)
+            {
+                Console.Clear();
+                Console.WriteLine("Invalid option, please enter a number between 1 and 5.");
+                Menu();
+            }
+
+            switch (option)
+            {
+                case 1:
+                    Sum();
+                    break;
+                case 2:
+                    Subtraction();
+                    break;
+                case 3:
+                    Multiplication();
+                    break;
+                case 4:
+                    Division();
+                    break;
+                case 5:
+                    Console.WriteLine("Thank you for using our services!");
+                    Exit();
+                    break;
+                default:
+                    Menu(); break;
+            }
+        }
+
+        static void Sum()
+        {
+            Console.Clear();
             Console.WriteLine("Enter the first number:");
             double num1 = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Enter the operator:");
-            string op = Console.ReadLine();
-
             Console.WriteLine("Enter the second number:");
             double num2 = Convert.ToDouble(Console.ReadLine());
-
-            if (op == "+")
-            {
-                Console.WriteLine(num1 + num2);
-            }
-            else if (op == "-")
-            {
-                Console.WriteLine(num1 - num2);
-            }
-            else if (op == "*")
-            {
-                Console.WriteLine(num1 * num2);
-            }
-            else if (op == "/")
-            {
-                Console.WriteLine(num1 / num2);
-            }
-            else
-            {
-                Console.WriteLine("Invalid operator");
-            }
-
-            Console.WriteLine("Press any key to exit...");
+            double result = num1 + num2;
+            Console.WriteLine($"Result: {result}");
             Console.ReadKey();
+            Menu();
+        }
+
+        static void Subtraction()
+        {
+            Console.Clear();
+            Console.WriteLine("Enter the first number:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the second number:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("");
+            double result = num1 - num2;
+            Console.WriteLine($"Result: {result}");
+            Console.ReadKey();
+            Menu();
+        }
+
+        static void Multiplication()
+        {
+            Console.Clear();
+            Console.WriteLine("Enter the first number:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the second number:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("");
+            double result = num1 * num2;
+            Console.WriteLine($"Result: {result}");
+            Console.ReadKey();
+            Menu();
+        }
+
+        static void Division()
+        {
+            Console.Clear();
+            Console.WriteLine("Enter the first number:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the second number:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("");
+            double result = num1 / num2;
+            Console.WriteLine($"Result: {result}");
+            Console.ReadKey();
+            Menu();
+        }
+
+        static void Exit()
+        {
+            Environment.Exit(0);
         }
     }
 }
--- obj/Debug/net8.0/Program.AssemblyInfo.cs
+++ obj/Debug/net8.0/Program.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Program")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f74467b7e9a0bdf99d114b8bee356ce8bcb3335c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Program")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Program")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
